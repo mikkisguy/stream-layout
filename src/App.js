5caaa6be121713d5deab4b40a165e1d92e3f4a74
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-07-05 23:29:16 +0300

    Removed test stuff and edited conditional

--- src/App.js
+++ src/App.js
@@ -1,76 +1,28 @@
 import GlobalStyle from "./styles/globalStyle";
 import styled from "styled-components";
 import { resolution } from "./styles/variables";
-import { useEffect } from "react";
-import { RefreshingAuthProvider } from "@twurple/auth";
-import {
-  TOKEN_DATA_KEY,
-  CLIENT_ID,
-  CLIENT_SECRET,
-  INITIAL_ACCESS_TOKEN,
-  INITIAL_REFRESH_TOKEN,
-} from "./constants.js";
-import { ApiClient } from "@twurple/api";
 import { Link, Outlet } from "react-router-dom";
 import { IS_DEVELOPMENT } from "./constants";
 
 const App = () => {
-  useEffect(() => {
-    const runTokenTest = false;
-
-    if (localStorage.getItem(TOKEN_DATA_KEY) === null) {
-      const initialTokenData = {
-        accessToken: INITIAL_ACCESS_TOKEN,
-        refreshToken: INITIAL_REFRESH_TOKEN,
-        expiresIn: 0,
-        obtainmentTimestamp: 0,
-      };
-
-      localStorage.setItem(TOKEN_DATA_KEY, JSON.stringify(initialTokenData));
-    }
-
-    if (runTokenTest) {
-      const testRefreshToken = async () => {
-        const tokenData = JSON.parse(localStorage.getItem(TOKEN_DATA_KEY));
-        const authProvider = new RefreshingAuthProvider(
-          {
-            clientId: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            onRefresh: (newTokenData) =>
-              localStorage.setItem(
-                TOKEN_DATA_KEY,
-                JSON.stringify(newTokenData)
-              ),
-          },
-          tokenData
-        );
-
-        const apiClient = new ApiClient({ authProvider });
-
-        const user = await apiClient.users.getUserByName("MikkisGuy");
-
-        console.log(user);
-      };
-
-      testRefreshToken();
-    }
-  }, []);
-
   return (
     <>
       <GlobalStyle />
       <StreamLayout>
         <Outlet />
       </StreamLayout>
-      <RouterLinks isDevelopment={IS_DEVELOPMENT}>
-        <Link to="/">Main</Link>
-        <Link to="fullwidth">Main full width</Link>
-        <Link to="chatting">Just chatting</Link>
-        <Link to="break">Break</Link>
-        <Link to="break-fullwidth">Break full width</Link>
-        <Link to="intro">Intro</Link>
-        <Link to="outro">Outro</Link>
-      </RouterLinks>
+
+      {IS_DEVELOPMENT && (
+        <RouterLinks>
+          <Link to="/">Main</Link>
+          <Link to="fullwidth">Main full width</Link>
+          <Link to="chatting">Just chatting</Link>
+          <Link to="break">Break</Link>
+          <Link to="break-fullwidth">Break full width</Link>
+          <Link to="intro">Intro</Link>
+          <Link to="outro">Outro</Link>
+        </RouterLinks>
+      )}
     </>
   );
 };
@@ -86,7 +38,6 @@ const StreamLayout = styled.div`
 `;
 
 const RouterLinks = styled.div`
-  display: ${({ isDevelopment }) => (isDevelopment ? "block" : "none")};
   padding: 15px;
 
   a {
