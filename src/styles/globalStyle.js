0742b927397d26fc41de685be51fdb0ef44da79f
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-07-05 04:20:31 +0300

    Added env check for background image

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -2,17 +2,18 @@ import { createGlobalStyle } from "styled-components";
 import minecraftScreenshot from "../assets/images/minecraft-screenshot.png";
 import cssReset from "./cssReset";
 import { resolution } from "./variables";
+import { ENV } from "../constants";
 
 const GlobalStyle = createGlobalStyle`
   ${cssReset}
 
   body  {
-    background-image: url('${minecraftScreenshot}');
+    background-image: ${
+      ENV === "development" ? `url('${minecraftScreenshot}')` : "none"
+    };
     background-size: ${resolution.w} ${resolution.h};
     background-repeat: no-repeat;
     background-color: #000;
-    width: ${resolution.w};
-    height: ${resolution.h};
   }
 `;
 
