606ea5c4f2d8604b7c477be45b5407b5ff415427
mikkisguy <82242822+mikkisguy@users.noreply.github.com>
2022-07-14 20:30:10 +0300

    Token api working

--- token-api/index.mjs
+++ token-api/index.mjs
@@ -1,30 +1,35 @@
 import express from "express";
-import * as path from "path";
-import * as fs from "fs";
-import { fileURLToPath } from "url";
 import jwt from "jsonwebtoken";
 const { sign: jwtSign } = jwt;
-
-export const DIR_NAME = path.dirname(fileURLToPath(import.meta.url));
-export const JWT = {
-  KEY_PATH: process.env.JWT_KEY_PATH,
-  ISSUER: "http://localhost:7070",
-  AUDIENCE: "http://localhost:7000",
-  ALGORITHM: "RS256",
-};
 
 const app = express();
 
 app.get("/token", async (_, res, next) => {
-  const fullPath = path.join(DIR_NAME, JWT.KEY_PATH);
+  try {
+    const jwtOptions = {
+      issuer: "http://localhost:7070",
+      audience: "http://localhost:7000",
+      expiresIn: "2m",
+    };
 
-  const jwtSecret = fs.readFileSync(fullPath);
+    const signedJwt = jwtSign({}, process.env.JWT_SECRET, jwtOptions);
 
-  const jwtOptions = {
-    issuer: JWT.ISSUER,
-    audience: JWT.AUDIENCE,
-    expiresIn: "2m",
-  };
-
-  res.send(jwtSign({}, jwtSecret, jwtOptions));
+    res.send({ token: signedJwt });
+  }
+  catch (error) {
+    next(error);
+  }
 });
+
+app.use((error, request, response, next) => {
+  if (response.headersSent) {
+    console.error("==> Headers already sent");
+    next(error);
+  }
+
+  console.error(`==> Request ${request.method} ${request.url} -> ${error.stack}`);
+
+  response.sendStatus(error.status || 500);
+});
+
+app.listen(7070, () => console.log("==> TOKEN SERVER RUNNING"));
