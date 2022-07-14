1262dd199281e363213f7dce4e2814b175bbf66a
mikkisguy <82242822+mikkisguy@users.noreply.github.com>
2022-07-14 23:44:42 +0300

    Edited build script, updated frontend env

--- local/frontend/src/constants.js
+++ local/frontend/src/constants.js
@@ -1,3 +1,3 @@
 export const IS_DEVELOPMENT = import.meta.env.DEV;
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const STREAM_API_URL = import.meta.env.VITE_STREAM_API_URL;
--- local/run-build.sh
+++ local/run-build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd /frontend
 yarn install
 yarn build
 docker build -t mikkisguy-stream-frontend .
+
+cd ../token-api
+docker build -t mikkisguy-stream-tokenapi .
