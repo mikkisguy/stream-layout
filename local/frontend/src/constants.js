02a69fedafa48fcbc775232405fbb221e4d3b5dc
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-07-18 20:47:54 +0300

    Envs

--- local/frontend/src/constants.js
+++ local/frontend/src/constants.js
@@ -1,3 +1,5 @@
-export const IS_DEVELOPMENT = import.meta.env.DEV;
+export const ENV = import.meta.env.NODE_ENV;
+export const IS_DEVELOPMENT = ENV === "development";
 
+export const TOKEN_API_URL = "http://localhost:7070/token";
 export const STREAM_API_URL = import.meta.env.VITE_STREAM_API_URL;
