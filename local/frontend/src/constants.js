00759ba0a6d38c2f9ba11386244b93937a09bfa6
mikkisguy <82242822+mikkisguy@users.noreply.github.com>
2022-07-31 15:47:40 +0300

    Moved token api url to envs

--- local/frontend/src/constants.js
+++ local/frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const IS_DEVELOPMENT = import.meta.env.DEV;
 
-export const TOKEN_API_URL = "http://localhost:7070/token";
+export const TOKEN_API_URL = import.meta.env.VITE_TOKEN_API_URL;
 export const STREAM_API_URL = import.meta.env.VITE_STREAM_API_URL;
 
 export const MINUTE = 60000;
